c8384da1ac013d720fbd97662e8b18e58977463e
Thais Hamilton <thaismha@gmail.com>
2023-09-20 12:51:43 -0300

    First commit

---
+++ src/main/kotlin/com/game/kalaha/KalahaApplication.kt
@@ -0,0 +1,11 @@
+package com.game.kalaha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KalahaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KalahaApplication>(*args)
+}
